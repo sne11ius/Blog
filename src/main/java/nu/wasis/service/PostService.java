355da924e0838f92bce04abc3ba74967fc6d1517
Cornelius Lilge <cornelius.lilge@gmail.com>
2013-03-03 01:17:39 +0100

    added commons-lang and PostService

--- src/main/java/nu/wasis/service/PostService.java
+++ src/main/java/nu/wasis/service/PostService.java
@@ -1,5 +1,23 @@
 package nu.wasis.service;
 
+import java.util.List;
+
+import nu.wasis.blog.model.Post;
+
+import org.apache.commons.lang.NotImplementedException;
+
 public class PostService {
+
+    public List<Post> getPosts() {
+        throw new NotImplementedException();
+    }
+
+    public Post getPost(final String postId) {
+        throw new NotImplementedException();
+    }
+
+    public void addPost(final Post post) {
+        throw new NotImplementedException();
+    }
 
 }
