e12a8ebcb6a4709fb48f1359c49699eb36ff417c
Cornelius Lilge <cornelius.lilge@gmail.com>
2013-03-04 02:47:01 +0100

    new url prefix for routes

--- src/main/java/nu/wasis/blog/Blog.java
+++ src/main/java/nu/wasis/blog/Blog.java
@@ -11,15 +11,17 @@ public class Blog {
     static final Logger LOG = Logger.getLogger(Blog.class);
     static final PostService postService = new PostService();
 
+    private static final String URL_PREFIX = "/mit/blog";
+
     /**
      * @param args
      */
     public static void main(final String[] args) {
         // postService.deleteAllPosts();
 
-        Spark.get(new IndexRoute("/"));
-        Spark.post(new ConnectRoute("/connect"));
-        Spark.post(new DisconnectRoute("/disconnect"));
-        Spark.post(new SubmitPostRoute("/submitpost"));
+        Spark.get(new IndexRoute(URL_PREFIX + "/"));
+        Spark.post(new ConnectRoute(URL_PREFIX + "/connect"));
+        Spark.post(new DisconnectRoute(URL_PREFIX + "/disconnect"));
+        Spark.post(new SubmitPostRoute(URL_PREFIX + "/submitpost"));
     }
 }
